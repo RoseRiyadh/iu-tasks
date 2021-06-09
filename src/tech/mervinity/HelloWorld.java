880e55803c8be4e043186b0fa506a0ac1de18ca5
rosecollins <immortalnumbc@gmail.com>
2021-06-10 02:20:48 +0300

    editing documented comment

--- src/tech/mervinity/HelloWorld.java
+++ src/tech/mervinity/HelloWorld.java
@@ -6,7 +6,13 @@ package tech.mervinity;
 public class HelloWorld {
     /**
      * Hello world class
-     * @param argv - boohoo
+     * implementation of task :
+     * There is an interface Vehicle which has a method honk();
+     * There is a class Car which implements this interface.
+     * Please write a java code which reflects this situation.
+     * Which annotation can you use here?
+     * A/ We use @Override to implement honk() method in Car class
+     * @param argv - Argument vector
      */
     public static void main(String[] argv) {
         Car car = new Car();
